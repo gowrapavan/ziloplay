10c40cb0ffb10db0a02ce42ea507eec0d5a7856f
gowrapavan <gowrapavankumar2004@gmail.com>
2026-01-05 21:50:57 +0530

    live

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,13 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { HelmetProvider } from 'react-helmet-async'; // <--- Import this
 import App from "./App";
 import "./index.css";
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <React.StrictMode>
-    <App />
+    <HelmetProvider> {/* <--- Wrap App with this */}
+          <App />
+    </HelmetProvider>
   </React.StrictMode>
 );
